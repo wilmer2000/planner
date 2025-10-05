4fe3b39143c92499e0ed0e431f315854ff159953
Wilmer Blanco <wilmervzla2000@gmail.com>
2025-10-05 12:10:31 -0300

    Initialize backend with Prisma setup, PostgreSQL configuration, Docker entrypoint, migrations, and updated `.gitignore`. (#1)

---
+++ backend/prisma/migrations/20251001032527_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
